d2aea6be24337dd5f855b9bfdde326017a11196c
Jonas Silva <jonas_profissional@outlool.com>
2022-09-07 21:41:11 -0300

    corrigi alguns bugs e alterei a cor e layout da pagina, porém o logo ainda esta bugado

--- src/script.js
+++
@@ -1,7 +0,0 @@
-const NavMenu = document.querySelector('.nav-menu');
-
-/*metodo para aplicar efeitos na pagina*/
-menu.addEventListener('click', () => {
-    
-    NavMenu.classList.toggle('ativo');
-})
