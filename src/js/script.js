697cc44b86bb20fa9ef71dd3b0e452cb4a1e01ba
Jonas Silva <jonas_profissional@outlool.com>
2022-09-12 21:27:56 -0300

    terminei a validacao do form com js

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,48 @@
-function Enviar() {
+const form = document.getElementById('form-js');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required');
+const emailRegex = /\S+@\S+\.\S+/;;
 
-    var nome = document.getElementById("nomeid");
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    nameValidate();
+    emailValidate();
+    numberValidate();
+});
 
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }else{
-        alert('Atenção ' + nome.value + ' os seus dados estão incorretos!');
+function setError(index) {
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'block';
+
+}
+
+function removeError(index) {
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+function nameValidate() {
+    if (campos[0].value.length < 3) {
+
+        setError(0);
+    } else {
+        removeError(0);
     }
+}
 
+function emailValidate() {
+    if (!emailRegex.test(campos[1].value)) {
+        setError(1);
+    } else {
+        removeError(1);
+    }
+}
+
+function numberValidate() {
+    if (campos[2].value.length < 11) {
+
+        setError(2);
+    } else {
+        removeError(2);
+    }
 }
